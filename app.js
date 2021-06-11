8d651fffddfb993f1cd35745cfe7bd9929b2b7d0
IDHEMEL <elmehdiouajjani@gmail.com>
2021-06-12 00:49:31 +0100

    commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const articlesRouter = require('./routes/articles');
+const commentsRouter = require('./routes/comments');
+const tagsRouter = require('./routes/tags');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/articles', articlesRouter);
+app.use('/comments', commentsRouter);
+app.use('/tags', tagsRouter);
+
+module.exports = app;
